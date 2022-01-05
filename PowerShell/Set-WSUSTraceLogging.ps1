a7fcda6ddab4ddf1d1a43c742ba225d3b2a7f2c4
Cody Mathis <cody.mathis123@gmail.com>
2022-01-05 10:31:54 -0500

    not a function...

--- PowerShell/Set-WSUSTraceLogging.ps1
+++ PowerShell/Set-WSUSTraceLogging.ps1
@@ -1,5 +1,4 @@
-function Set-WSUSTraceLogging {
-    <#
+<#
 .SYNOPSIS
     Enable or Disable WSUS client (Windows Update Agent) trace logging.
 .DESCRIPTION
@@ -16,41 +15,40 @@ function Set-WSUSTraceLogging {
     This script makes modifications to the registry. Modifying REGISTRY settings incorrectly can cause serious problems that may prevent your computer from booting properly. 
     Patch My PC cannot guarantee that any problems resulting from the configuring of REGISTRY settings can be solved. Modifications of these settings are at your own risk.
 #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('Enabled', 'Disabled')]
-        [string]$TraceLogging = 'Enabled'
-    )
-    begin {
-        $rootWUPath = 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate'
-        $tracePath = "$rootWUPath\Trace"
-        $restartService = $true
-    }
-    process {
-        switch ($TraceLogging) {
-            Enabled {
-                if (-not (Test-Path $tracePath)) {
-                    $null = New-Item -Path $rootWUPath -Name Trace -ItemType Directory
-                }
-                Set-ItemProperty -Path $tracePath -Name Flags -Value 7 -Force
-                Set-ItemProperty -Path $tracePath -Name Level -Value 4 -Force
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Enabled', 'Disabled')]
+    [string]$TraceLogging = 'Enabled'
+)
+begin {
+    $rootWUPath = 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate'
+    $tracePath = "$rootWUPath\Trace"
+    $restartService = $true
+}
+process {
+    switch ($TraceLogging) {
+        Enabled {
+            if (-not (Test-Path $tracePath)) {
+                $null = New-Item -Path $rootWUPath -Name Trace -ItemType Directory
             }
-            Disabled {
-                if (Test-Path $tracePath) {
-                    Remove-Item -Path $tracePath -Recurse -Force
-                }
-                else {
-                    Write-Warning 'Trace logging is already disabled'
-                    $restartService = $false
-                }
+            Set-ItemProperty -Path $tracePath -Name Flags -Value 7 -Force
+            Set-ItemProperty -Path $tracePath -Name Level -Value 4 -Force
+        }
+        Disabled {
+            if (Test-Path $tracePath) {
+                Remove-Item -Path $tracePath -Recurse -Force
+            }
+            else {
+                Write-Warning 'Trace logging is already disabled'
+                $restartService = $false
             }
         }
     }
-    end {
-        if ($restartService) {
-            Restart-Service -Name wuauserv
-            Write-Host "Trace logging is now $TraceLogging"
-        }
+}
+end {
+    if ($restartService) {
+        Restart-Service -Name wuauserv
+        Write-Host "Trace logging is now $TraceLogging"
     }
 }
