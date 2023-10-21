5395f7a04e3d7d3268bbb1025203f7987fb1f4f8
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2023-10-21 12:16:20 +0100

    Update Get-LocalContentHashes.ps1
    Minor tweaks to verbose output

--- PowerShell/Get-LocalContentHashes.ps1
+++ PowerShell/Get-LocalContentHashes.ps1
@@ -11,9 +11,11 @@
     guarantee that the following script, macro, or code can or should be used in any situation or that 
     operation of the code will be error-free.
 #>
-
 $pmpreg = 'SOFTWARE\Patch My PC Publishing Service'
+$VerbosePreference = 'Continue'
+
 Function Get-EncodedHash {
+    [CmdletBinding()]
     Param(
         [Parameter(Position = 0)]
         [System.Object]$hashValue
@@ -41,6 +43,8 @@ Catch {
 # Get all .exe and .msi files in the Local Content Repository
 $resultArray = @()
 $files = Get-ChildItem -Path $localContentRepo -Recurse -File -ErrorAction SilentlyContinue | Where-Object { $_.Name -like "*msi" -or $_.Name -like "*exe" }
+Write-Verbose -Message ("Found {0} files in the Local Content Repository. Getting file information, please wait..." -f $($files.Count))
+
 Foreach ($file in $files) {
 
     $msiVersion = $Null
@@ -59,7 +63,7 @@ Foreach ($file in $files) {
             $view.GetType().InvokeMember("Close", "InvokeMethod", $Null, $view, $Null)
         }
         Catch {
-            Write-Verbose ("Failed to get MSI file version for ""$($file.FullName)"". {0}" -f $_)
+            Write-Warning ("Failed to get MSI file version for ""$($file.FullName)"". {0}" -f $_)
         }
     }
 
@@ -75,16 +79,18 @@ Foreach ($file in $files) {
         MSIVersion     = $msiVersion
         Hash           = $encodedhash
     }
-    $result
     $resultArray += $result
 }
+
+# Output result to console
+$resultArray | Format-Table -AutoSize
 
 # Export result to CSV
 $resultArray | Export-Csv -Path $localContentRepo\LocalContentHashes.csv -NoTypeInformation
 
-If (Test-Path -Path localContentRepo\LocalContentHashes.csv) {
-    Write-Verbose ('CSV file succesfully exported to {0}' -f "$localContentRepo\LocalContentHashes.csv")
+If (Test-Path -Path $localContentRepo\LocalContentHashes.csv) {
+    Write-Verbose -Message ("CSV file succesfully exported to '{0}'" -f "$localContentRepo\LocalContentHashes.csv") 
 }
 Else {
-    Write-Verbose ('Failed to export CSV file to {0}' -f "$localContentRepo\LocalContentHashes.csv")
+    Write-Verbose -Message ("Failed to export CSV file to '{0}'. Check your account has permissions to write to that location." -f "$localContentRepo\LocalContentHashes.csv")
 }
